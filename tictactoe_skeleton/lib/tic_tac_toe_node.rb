6d031b6257f94d30dc646e0e08d47e958e32ab08
Kyle Chen <kyle.h.chen@gmail.com>
2017-10-06 16:52:15 -0700

    Write children method TicTacToeNode

---
+++ tictactoe_skeleton/lib/tic_tac_toe_node.rb
@@ -0,0 +1,47 @@
+require_relative 'tic_tac_toe'
+
+class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
+  def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
+  end
+
+  def losing_node?(evaluator)
+  end
+
+  def winning_node?(evaluator)
+  end
+
+  # This method generates an array of all moves that can be made after
+  # the current move.
+  def children
+    next_marker = [:x,:o].reject {|mark| mark == @next_mover_mark}.first
+    @children = []
+
+    (0..2).each do |x|
+      (0..2).each do |y|
+        pos = [x,y]
+        if @board.empty?(pos)
+          dupboard = @board.dup
+          dupboard[pos] = @next_mover_mark
+
+          prev_move_pos = pos
+          @children << TicTacToeNode.new(dupboard, next_marker, prev_move_pos)
+        end
+      end
+    end
+    @children
+  end
+
+  def switch_mark
+    if @next_mover_mark == :x
+      @next_mover_mark = :o
+    else
+      @next_mover_mark = :x
+    end
+  end
+
+end
